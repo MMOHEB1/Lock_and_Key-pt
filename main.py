d9334a0b222a91ddc2247bd29e397608d53b5371
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-05-24 18:28:50 +0100

    commit 3:

--- main.py
+++ main.py
@@ -13,10 +13,21 @@ from tkinter import *
 #         security_file.write(f"\n {web_name} | {username} | {password}")
 #     print(security_file)
 
-def same_file(web_name, username, password):
+def save():
+
+    website1 = website_input.get()
+    email1 = email_input.get()
+    pswrd1 = pswrd_input.get()
+
     with open("data.txt", "a") as security_file:
-        security_file.write(f"\n {web_name} | {username} | {password}")
-    print(security_file.read())
+        security_file.write(f"\n {website1} | {email1} | {pswrd1}\n")
+        website_input.delete(0, END)
+        pswrd_input.delete(0, END)
+
+# def same_file(web_name, username, password):
+#     with open("data.txt", "a") as security_file:
+#         security_file.write(f"\n {web_name} | {username} | {password}")
+#     print(security_file.read())
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -58,12 +69,12 @@ pswrd_input.grid(column=1, row=3, sticky='nsew')
 generate_pswrd = Button(text="Generate Password")
 generate_pswrd.grid(column=2, row=3)
 
-def new_file():
-    with open("data.txt", "x") as security_file:
-        security_file.write(f"\n {website_input.get()} | {email_input.get()} | {pswrd_input.get()}")
-    print(security_file.read())
+# def new_file():
+#     with open("data.txt", "x") as security_file:
+#         security_file.write(f"\n {website_input.get()} | {email_input.get()} | {pswrd_input.get()}")
+#     print(security_file.read())
 
-add = Button(text="Add", command=new_file)
+add = Button(text="Add", command=save)
 add.grid(column=0, row=4, columnspan=3, sticky='nsew')
 
 # button_perss:
