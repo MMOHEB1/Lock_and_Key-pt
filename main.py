d788e27e0436b8acc0690af271735e0e866929a3
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-05-28 21:09:32 +0100

    commit 3.2: had to do a reshuffle because my file wasnt running properly.

--- main.py
+++ main.py
@@ -4,41 +4,41 @@ from random import choice, randint, shuffle
 import pyperclip
 import json
 
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+#Password Generator Project
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    password_letter = [choice(letters) for _ in range(randint(8, 10))]
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-    password_list = password_letter + password_symbols + password_numbers
+    password_list = password_letters + password_symbols + password_numbers
     shuffle(password_list)
 
     password = "".join(password_list)
-    pswrd_input.insert(0, password)
+    password_entry.insert(0, password)
     pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
 def save():
-    website1 = website_input.get()
-    email1 = email_input.get()
-    pswrd1 = pswrd_input.get()
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
     new_data = {
-        website1: {
-            "Email": email1,
-            "Password": pswrd1
+        website: {
+            "Email": email,
+            "Password": password
         }
     }
 
-    if len(website1) == 0 or len(pswrd1) == 0:
-        messagebox.showinfo(title="OOP", message="Please do not leave any fields empty")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="OOPS!", message="Please do not leave any fields empty")
     else:
         try:
             with open("data.json", "r") as security_file:
@@ -56,52 +56,43 @@ def save():
                 json.dump(data, security_file, indent=4)
         finally:
             # deletes the inputs of the website and password once add is clicked
-            website_input.delete(0, END)
-            pswrd_input.delete(0, END)
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-
 window = Tk()
-window.config(pady=20, padx=20)
 window.title("Password Manager")
-img_file = PhotoImage(file="logo.png")
-canvas = Canvas(width=200, height=200, highlightthickness=0)
-canvas.create_image(100, 100, image=img_file)
-canvas.grid(column=1, row=0)
+window.config(padx=50, pady=50)
 
-# Website label:
-website = Label(text="Website:")
-website.grid(column=0, row=1)
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
 
-website_input = Entry(window)
-website_input.grid(column=1, row=1)
-website_input.focus()
+#Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
 
-# Email label:
-email = Label(text="Email/Username:")
-email.grid(column=0, row=2)
+#Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "angela@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
-email_input = Entry(window)
-email_input.grid(column=1, row=2, columnspan=2, sticky='nsew')
-email_input.insert(0, "mohebimustafa99@gmail.com")
-
-# Password label:
-pswrd = Label(text="Password:")
-pswrd.grid(column=0, row=3)
-
-pswrd_input = Entry(window)
-pswrd_input.grid(column=1, row=3, sticky='nsew')
-
-# Buttons:
-generate_pswrd = Button(text="Generate Password", command=generate_password)
-generate_pswrd.grid(column=2, row=3)
-
-search = Button(text="Search")
-search.grid(column=2, row=1, sticky='ew')
-
-add = Button(text="Add", command=save)
-add.grid(column=1, row=4, columnspan=3, sticky='nsew')
+# Buttons
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
